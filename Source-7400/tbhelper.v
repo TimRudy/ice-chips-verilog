4d28646c51677ee34612392c2da30ac9e11cea6a
Tim Rudy <3942818+TimRudy@users.noreply.github.com>
2018-02-19 22:24:44 -0500

    Added variant

--- Source-7400/tbhelper.v
+++ Source-7400/tbhelper.v
@@ -4,3 +4,5 @@
 `define ASSIGN_UNPACK(PK_WIDTH, PK_LEN, UNPK_IN_BUS, PK_DATA) wire [PK_WIDTH*PK_LEN-1:0] IN_BUS; assign IN_BUS = PK_DATA; genvar unpk_idx; generate for (unpk_idx=0; unpk_idx<PK_LEN; unpk_idx=unpk_idx+1) begin assign UNPK_IN_BUS[unpk_idx][(PK_WIDTH-1):0] = IN_BUS[(PK_WIDTH*unpk_idx+PK_WIDTH-1):(PK_WIDTH*unpk_idx)]; end endgenerate
 
 `define TBASSERT_METHOD(TB_NAME) reg [512:0] tbassertLastPassed = ""; task TB_NAME(input condition, input [512:0] s); if (condition === 1'bx) $display("-Failed === x value: %-s", s); else if (condition == 0) $display("-Failed: %-s", s); else if (s != tbassertLastPassed) begin $display("Passed: %-s", s); tbassertLastPassed = s; end endtask
+
+`define TBASSERT_2_METHOD(TB_NAME) reg [512:0] tbassert2LastPassed = "", f; task TB_NAME(input condition, input [512:0] s, input integer major, input [512:0] minor); $sformat(f, "%0s %0d-%0s", s, major, minor); if (condition === 1'bx) $display("-Failed === x value: %-s", f); else if (condition == 0) $display("-Failed: %-s", f); else if (f != tbassert2LastPassed) begin $display("Passed: %-s", f); tbassert2LastPassed = f; end endtask
